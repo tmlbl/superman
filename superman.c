728ddcf1cdc4ad51e4251b675dff91d0748102dc
Tim Lebel <tim.lebel@gmail.com>
2015-12-12 01:58:28 -0800

    Progress

--- superman.c
+++ superman.c
@@ -1,40 +1,52 @@
 #include <yaml.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <assert.h>
 #include <time.h>
 
 const int MAX_PROCESSES = 100;
+int use_color = 0;
+
+const int S_COMMAND = 2;
 
 struct process {
   char *cmd; // The command to be run
-}
+};
 
-void print_with_color(int color, char *msg)
+void print_with_color(int color, const char *msg, ...)
 {
   time_t timer;
   char buffer[26];
+  char msg_buf[128];
   struct tm* tm_info;
+  va_list ap;
+  int i;
+
+  va_start(ap, msg);
+  vsnprintf(msg_buf, 100, msg, ap);
+  va_end(ap);
 
   time(&timer);
   tm_info = localtime(&timer);
 
   strftime(buffer, 26, "%Y/%m/%d %H:%M:%S", tm_info);
 
-  if (isatty(fileno(stdout)))
+  if (use_color)
   {
-    printf("\x1B[35m[\x1B[36m%s\x1B[35m] \x1B[%dm%s\x1B[0m\n", buffer, color, msg);
+    printf("\x1B[35m[\x1B[36m%s\x1B[35m] \x1B[%dm%s\x1B[0m\n", buffer, color, msg_buf);
   }
   else
   {
-    printf("[%s] %s\n", buffer, msg);
+    printf("[%s] %s\n", buffer, msg_buf);
   }
 }
 
 int main(int argc, char *argv[])
 {
     int number;
+    use_color = isatty(fileno(stdout));
 
     print_with_color(33, "Started superman");
 
@@ -65,6 +77,7 @@ int main(int argc, char *argv[])
         yaml_parser_set_input_file(&parser, file);
 
         int block_depth = 0;
+        int last_tag;
 
         while (!done)
         {
@@ -77,17 +90,31 @@ int main(int argc, char *argv[])
             {
               case YAML_MAPPING_START_EVENT:
                 block_depth++;
-                printf("Mapping start %d\n", block_depth);
                 break;
               case YAML_MAPPING_END_EVENT:
                 block_depth--;
-                printf("Depth %d\n", block_depth);
                 break;
               case YAML_SCALAR_EVENT:
-                printf("Value: %s\n", event.data.scalar.value);
+                if (block_depth == 1)
+                {
+                  print_with_color(33, "Creating process: %s", event.data.scalar.value);
+                }
+
+                // Store the value in a buffer
+                char buf[128];
+                sprintf(buf, "%s", event.data.scalar.value);
+
+                // Based on last tag
+                if (last_tag == S_COMMAND)
+                {
+                  printf("Command Value: %s\n", buf);
+                  system(buf);
+                }
+
+                if (buf[0] == 'c') last_tag = S_COMMAND;
                 break;
               default:
-                printf("Event type: %d\n", event.type);
+                // printf("Event type: %d\n", event.type);
                 break;
             }
 
